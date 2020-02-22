1232b0d4089c2f65fed9a875fe945aa0112d1371
Dmitry Osipenko <digetx@gmail.com>
2020-02-23 00:25:30 +0300

    partitions: Introduce NVIDIA Tegra Partition Table
    All NVIDIA Tegra devices use a special partition table format for the
    internal storage partitioning. Most of Tegra devices have GPT partition
    in addition to TegraPT, but some older Android consumer-grade devices do
    not or GPT is placed in a wrong sector, and thus, the TegraPT is needed
    in order to support these devices properly in the upstream kernel. This
    patch adds support for NVIDIA Tegra Partition Table format that is used
    at least by all NVIDIA Tegra20 and Tegra30 devices.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

---
+++ block/partitions/tegra.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __FS_PART_TEGRA_H__
+#define __FS_PART_TEGRA_H__
+
+int tegra_partition(struct parsed_partitions *state);
+
+#endif /* __FS_PART_TEGRA_H__ */
